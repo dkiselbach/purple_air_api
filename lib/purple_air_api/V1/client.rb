a1d6d2de2e92ead2b2e325b77f65c5411a8139ae
dkiselbach <dylankiselbach@gmail.com>
2021-02-26 19:03:56 -1000

    Add yard docs. Start documenting

--- lib/purple_air_api/V1/client.rb
+++ lib/purple_air_api/V1/client.rb
@@ -4,19 +4,26 @@ require 'faraday'
 
 module PurpleAirApi
   module V1
-    # Client class for interfacing with the V1 PurpleAir API
+    # Client class for interfacing with the V1 PurpleAir API. Refer to the instance methods to learn more about what
+    # methods and API options are available.
     class Client
       API_URL = 'https://api.purpleair.com/v1/'
 
       attr_reader :read_client, :write_client
 
-      # @example
-      #
+      # Creates a read and write client to interface with the Purple Air API.
+      # @!method initialize(read_token:, write_token:)
+      # @param read_token [String] The read client you received from PurpleAir
+      # @param write_token [String] The write client you received from PurpleAir
 
       def initialize(read_token:, write_token:)
         @read_client = create_http_client(read_token)
         @write_client = create_http_client(write_token)
       end
+
+      # Makes a request to the sensors endpoint and returns a parsed JSON.
+      # @!method request_sensor_data
+      #
 
       def request_sensor_data
         GetSensors.call(client: read_client)
